cd3483ba509a63e12a8e2a3c2d081769a318c873
jobanjit-singh <singhjobanjit02@gmail.com>
2023-05-13 18:35:27 +0530

    Introduction to Python

---
+++ one.py
@@ -0,0 +1,30 @@
+# a = 4
+# print("The value of a is",a)
+
+# a = eval(input("Enter the value of a:- "))
+# b = eval(input("Enter the value of b:- "))
+# c = True
+# print(a+b)
+# print(c)
+
+# operators:-
+# Arithemetic Operators:- + - * / % // **
+# Comparison Operators:- > < >= <= == !=
+# Assignment Operator:- ( = )
+# Logical Operator:- and or not
+
+# Even Odd Program:- 
+a = eval(input("Enter the number:- ")) # a = 3
+
+ans = a % 2 == 0 # ans 2 % 2 -> 0 == 0 -> True
+
+print("the condition ans :- ",ans)
+if(ans == True):
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
+# print("this is even odd program")
+# if(a % 2 != 0):
+#     print("Number is Odd")
+
