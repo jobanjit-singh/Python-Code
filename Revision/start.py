2155187688f9d7779610fbdaa4b401da455659b5
jobanjit-singh <singhjobanjit02@gmail.com>
2023-11-23 12:54:29 +0530

    Revision what is programming

---
+++ Revision/start.py
@@ -0,0 +1,32 @@
+# what is Programming?
+# Variables
+# Python Literals 
+    # Numeric Literals
+    # String Literals
+    # Boolean Literals
+# Comment
+# Python Input and Output
+# python Print()
+# Type Conversion
+
+# print('Hello World',end='-')
+
+# a = 12
+# b = 4.5
+# c = "hello World"
+# d = True # False
+# print('The value of a is : ',a)
+# print('The value of b :',b)
+# print('The value of c: ',c)
+# print('The value of d is :',d)
+
+# input_value = input('Enter the value of variable: ')
+# print('The Entered value type is :',type(input_value))
+
+a = eval(input("Enter the value of a: "))
+b = eval(input("enter the value of b: "))
+# a = input("Enter the value of a:")
+# b = input("enter the value of b:")
+# c = int(a) + int(b)
+c = a + b
+print("the result is: ",c)
