473ab531f4986b477b8dfd17f4c107ec9c07769e
jobanjit-singh <singhjobanjit02@gmail.com>
2023-11-27 13:12:30 +0530

    Type Casting and Operators in Python

---
+++ Revision/Operator.py
@@ -0,0 +1,143 @@
+# Implicit Conversion done
+# Explicit Conversion done
+# type() function done
+# String to int done
+# int and float to string done
+# Error during type conversion done
+# Loss of information during conversion done
+
+# Operator
+# what Operator?
+# Arithemetic operator
+# + & + with string
+# -
+# * & * multiply String
+# / and // operator
+# % operator
+# ** operator
+# Assignment opertor and compound operator
+# operator Precedence
+
+#---------------------------------------------------
+
+# a = 10
+# b = 12.5
+
+# c = a + b
+
+# print("The sum of two numbers is:- ",c)
+
+# print(type(c))
+
+# ---------------------------------------------------
+
+# string = """ jit code is a youtube 
+
+# channel"""
+
+# s = "jit code is a 
+# youtube channel"
+
+#--------------------------------------------------
+
+# a = 'hello'
+
+# new_a = int(a)
+# # new_a = eval(a)
+
+# print(type(new_a))
+
+#---------------------------------------------------
+
+# a = 23
+# b = 14.6
+
+# string_a = str(a)
+# string_b = str(b)
+
+# print(type(string_a))
+# print(type(string_b))
+#---------------------------------------------------
+
+# a = '15.5' # string of float number
+# float_a = int(a)
+
+# print(float_a)
+
+# error 1
+
+
+# ------------------------------------------------
+
+# a = 13.73658
+# integer_a = int(a)
+
+# print(integer_a)
+
+# --------------------------------------------------
+# Operator
+# what Operator? done
+# Arithemetic operator
+# + & + with string done
+# - done
+# * & * multiply String done
+# / and // operator done
+# % operator done
+# ** operator done
+# Assignment opertor and compound assignment operator done
+# operator Precedence 
+
+#-----------------------------------------------------
+# addition Operator
+# a = 10
+# b = 20
+# c = a + b
+
+# str1 = "Hello"
+# str2 = "world"
+# new_str = str1 + " " + str2 # Hello + " " + world
+
+#----------------------------------------------------
+# subtraction operator
+# a = 10-23
+# print(a)
+
+#----------------------------------------------------
+# multiplication operator
+# a = 12 * 2
+# b = 12.5 * 2
+# str1 = "Ira "
+# new_string = str1*3
+# print(new_string)
+
+#-----------------------------------------------------
+# Division Operator
+# a = 13
+# b = 2
+
+# c = a / b
+# c1 = a // b
+# print(c)
+# print(c1)
+
+# modulas
+# a = 13%2
+# print(a)
+
+#------------------------------------------------------------
+# Exponential Operator (**)
+
+# a = 2 ** 3 # 2 * 2 * 2 
+# print(a)
+
+#--------------------------------------------------------------
+# Assingment Operator and Compound Assignment Operator
+# a = 10
+# # a = a + 5
+
+# a += 5
+
+#---------------------------------------------------------------
+# Operator Precedence
+# a = (14 ** 2 * (3 - 2))
+# print(a)
