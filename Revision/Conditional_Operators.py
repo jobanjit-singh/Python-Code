16e300565c260eeb810fc54a7f086976966cd055
jobanjit-singh <singhjobanjit02@gmail.com>
2023-11-28 19:08:23 +0530

    Comparison and logical operators

---
+++ Revision/Conditional_Operators.py
@@ -0,0 +1,47 @@
+# Boolean Expression done
+# Comparison Operator done
+    # > < >= <= == !=
+# Examples of Comparison Operators done
+# Logical Operator done
+    # and or not
+# If Statements
+# if-else Statements
+# if-elif-else Statements
+# Nested if-else Statements
+
+#-------------------------------------------
+# boolean expressions
+# a = 10
+# b = 20
+# c = a > b
+# print(c)
+
+#--------------------------------------------
+# Comparison Operators or Conditional Operators
+
+# a = 20
+# b = 20
+
+# print(a > b) #False
+# print(a < b) #False
+# print(a <= b) #True
+# print(a >= b) #True
+# print(a == b) #True
+# print(a != b) #False
+
+# a = 30
+# b = 15
+# print(b >= a) #False
+
+#---------------------------------------------------
+# Logical Operator
+# and or not
+
+a = "admin"
+name = "Hello"
+
+# print(a == "admin" and name == "Hello") #True
+# print(a == "admin" or name == "hello") # True
+
+# print(not name == "Hello") # False
+
